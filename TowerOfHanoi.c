3810de189334c332647d74138c864272fde299d3
Jay1425 <jayraychura13@gmail.com>
2024-09-20 22:48:59 +0530

    toh

---
+++ TowerOfHanoi.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void TOH(int n, int A, int B, int C)
+{
+    if(n>0)
+    {
+        TOH(n-1, A, C, B);
+        printf("move a disk %d to %d\n", A, C);
+        TOH(n-1, B, A, C);
+    }
+}
+
+int main()
+{
+    TOH(3, 1, 2, 3);
+
+    return 0;
+}
